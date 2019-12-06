8a9cb71e180f95a75b6a8904119fcaaa53339c04
Henry Soule <hsoule427@gmail.com>
2019-12-05 19:26:16 -0700

    Currently has a test case

--- src/diff_evo_driver.py
+++ src/diff_evo_driver.py
@@ -28,11 +28,23 @@ if db.get_dataset_type() == 'classification':
                     5, 5,                       # (2)
                     len(db.get_class_list())]   # (3)
 
+
     # This number is arbitrary.
     # TODO: Tune this per dataset
     learning_rate = 1.5
 
-    diff_evo.main_loop(db, layer_sizes, learning_rate, generations=1000)
+    ffnn = FFNN(db.get_data(), learning_rate)
+
+
+
+    # Make weights and biases
+    w_set = np.random.randn(sf.calc_total_vec_length(layer_sizes))
+    weights, biases = sf.encode_weight_and_bias(w_set, layer_sizes)
+    ffnn.set_weight(weights)
+    ffnn.set_biases(biases)
+
+    # diff_evo.main_loop(db, layer_sizes, learning_rate, generations=1000)
+    ffnn.get_fitness(sf.classification_error)
 
 
 
@@ -40,6 +52,12 @@ if db.get_dataset_type() == 'classification':
 
 # BEGIN regression FFNN
 elif db.get_dataset_type() == 'regression':
+
+    process_data.FFNN_encoding(db)
+
+    print(db.get_data())
+    
+    learning_rate = 1.5
 
     # (1) First layer (input layer) has 1 node per attribute.
     # (2) Hidden layers has arbitrary number of nodes.
@@ -49,6 +67,19 @@ elif db.get_dataset_type() == 'regression':
         5, 5,               # (2)
         1                   # (3)
     ]
+
+    # ffnn = FFNN(db.get_data()[0], learning_rate)
+    
+    # Make weights and biases
+    # w_set = np.random.randn(sf.calc_total_vec_length(layer_sizes))
+    # weights, biases = sf.encode_weight_and_bias(w_set, layer_sizes)
+    # ffnn.set_weight(weights)
+    # ffnn.set_biases(biases)
+
+    # ffnn.get_fitness()
+
+    
+
 
 else:
     print('Database type invalid. Type = ' + db.get_dataset_type())
